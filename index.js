4d52846cacc18ddf4781b7acaa4d3bc69e3fb72a
Jan Stasek <jan.stasek@email.cz>
2023-05-21 21:11:57 +0200

    1st working version

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-import database from "./src/database/projects.json" assert { type: 'json' };
+import database from "./src/database/projects.json" assert { type: "json" };
+import { Color } from "three";
+import { IfcViewerAPI } from "web-ifc-viewer";
 
 //console.log(database);
 
@@ -8,38 +10,65 @@ let projects = database.projects;
 
 const projectCard = document.getElementById("project-card-container");
 
-//console.log(projectCard);
+console.log(projectCard);
 
-for(const project of projects) {
+if (projectCard !== null) {
+  for (const project of projects) {
     const card = createCard(project);
     projectCard.appendChild(card);
+  }
 }
 
 let lastCard;
 
 function createCard(project) {
-    const projectName = document.createElement("h4");
-    projectName.textContent = project["projectName"];
+  const projectName = document.createElement("h4");
+  projectName.textContent = project["projectName"];
 
-    const projectInfo = document.createElement("p");
-    projectInfo.textContent = project["projectDescription"]
+  const projectInfo = document.createElement("p");
+  projectInfo.textContent = project["projectDescription"];
 
-    const card = document.createElement("a");
-    card.classList.add("card");
-    const link = "viewer.html" + "?id=" + project["projectId"];
-    card.href = link;
-    card.target ="_blank";
-    card.appendChild(projectName);
-    card.appendChild(projectInfo);
+  const card = document.createElement("a");
+  card.classList.add("card");
+  const link = "viewer.html" + "?id=" + project["projectId"];
+  card.href = link;
+  card.target = "_blank";
+  card.appendChild(projectName);
+  card.appendChild(projectInfo);
 
-    card.addEventListener("click", () => {
-        if(lastCard) {
-            lastCard.classList.remove("selected-card");
-        }
+  card.addEventListener("click", () => {
+    if (lastCard) {
+      lastCard.classList.remove("selected-card");
+    }
 
-        card.classList.add("selected-card");
-        lastCard = card;
-    })
+    card.classList.add("selected-card");
+    lastCard = card;
+  });
 
-    return card;
+  return card;
 }
+
+let params = new URLSearchParams(document.location.search);
+let id = params.get("id");
+
+//console.log(id);
+
+const link = projects[id]["projectIfcRoute"];
+
+//console.log(link);
+
+const container = document.getElementById("viewer-container");
+const viewer = new IfcViewerAPI({
+  container,
+  backgroundColor: new Color(0xffffff),
+});
+viewer.grid.setGrid();
+viewer.axes.setAxes();
+
+async function loadIfc(url) {
+  await viewer.IFC.setWasmPath("../../../");
+  const model = await viewer.IFC.loadIfcUrl(url);
+  viewer.shadowDropper.renderShadow(model.modelID);
+}
+
+loadIfc(link);
--- viewer.js
+++
@@ -1,4 +0,0 @@
-let params = new URLSearchParams(document.location.search);
-let id = params.get("id");
-
-console.log(id);
